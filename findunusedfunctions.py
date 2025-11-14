4e31b8c42bffd81b11b78005f87b3b60163d5e8e
Your Name <you@example.com>
2025-11-14 01:32:12 -0800

    clean and args

--- findunusedfunctions.py
+++ findunusedfunctions.py
@@ -11,22 +11,25 @@ def get_all_files_content():
 	for filename in filenames:
 		try:
 			tmp = open(filename, 'r').read().splitlines()
-			content += "\n".join(tmp)
+			content += "\n".join(tmp) + "\n"
 		except Exception as e: print(e); exit()
-	print(content)
-	for name in filenames:
-		print(name.resolve())
-	#print(filenames)
-
-def giveContent(filename):
-	content = open(filename, "r").read().splitlines()
+	content = content.splitlines()
 	return [line for line in content if '#' not in line]
 
+def giveContent(filename):
+	try:
+		content = open(filename, "r").read().splitlines()
+		return [line for line in content if '#' not in line]
+	except Exception as e:
+		print(e);
+
 def giveDefs(content):
+	if content == None: return []
 	defs = [line.split()[1] for line in content if 'def ' in line]
 	return [part[:part.index('(')] for part in defs]
 
 def giveFuncCalls(content):
+	if content == None: return []
 	return [line[:line.index('(')] for line in content if '(' in line and 'def' not in line]
 
 def giveUnusedFuncs(defs, func_calls):
@@ -34,47 +37,12 @@ def giveUnusedFuncs(defs, func_calls):
 
 def findunusedfunctions(filename):
 	content = giveContent(filename)
+	#content = get_all_files_content()
 	unused_funcs = giveUnusedFuncs(giveDefs(content), giveFuncCalls(content))
 	print("Unused Functions:", unused_funcs)
 
 if __name__ == "__main__":
-	get_all_files_content()
-	#findunusedfunctions('testprogram.py')
-
-"""
-import sys
-from pathlib import Path
-
-def find_line_after_imports(txt):
-	imports = False
-	for i, line in enumerate(txt):
-		if "import" in line: imports = True
-		if "import" not in line and len(line) and imports: return i;
-
-def loopfiles():
-	content = []
-	if len(sys.argv) < 2: print("Give Folder in Args"); exit()
-	elif len(sys.argv) == 2: filenames = Path(sys.argv[1] + '/').rglob('*.py')
-	else: filenames = set(sys.argv[1:])
-	for filename in filenames:
-		try:
-			tmp = open(filename, 'r').read().splitlines()
-			content += tmp[:find_line_after_imports(tmp)]
-		except Exception as e: print(e); exit()
-	content = [line for line in content if 'import' in line and line[0] != '#']
-	content = [line[:line.find('as')] if 'as' in line else line for line in content]
-	content = [line[:line.find('import')] if ',' in line else line for line in content]
-	return " ".join(content)
-
-def fiximports():
-	imports = loopfiles()
-	imports = [line.replace(',', '') for line in ''.join(imp for imp in imports).split()]
-	imports = [line.strip() for line in imports if line != 'import' and line != 'from' and line != '*']
-	imports = set(imports)
-	savedcontent = "\n".join(imports)
-	open("requirements.txt", "w").write(savedcontent)
-	print("Created requirements.txt for:\n", savedcontent)
-
-if __name__ == "__main__": fiximports();
-find_line_after_imports(open("testfolder/test.py", 'r').read().splitlines())
-"""
+	ac = len(sys.argv)
+	if ac > 1:
+		for i in range(1, ac): findunusedfunctions(sys.argv[i])
+	else: print('Give files in args'); exit()
